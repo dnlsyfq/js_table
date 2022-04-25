23d187a9b60f09e42381ee17aa7250b32d592ce1
= <dan90@live.com.my>
2022-04-25 13:37:59 +0800

    'update'

---
+++ script.js
@@ -0,0 +1,15 @@
+function buildTable(data){
+    var table = document.getElementById('myTable')
+    table.innerHTML = ""
+    for(var i = 0; i < data.length; i++){
+        var row = `<tr>
+                    <td>${data[i].name}</td>
+                    <td>${data[i].age}</td>
+                    <td>${data[i].birthdate}</td>
+                   </tr>`
+
+        table.innerHTML += row;
+    }
+}
+
+buildTable(myArray);
